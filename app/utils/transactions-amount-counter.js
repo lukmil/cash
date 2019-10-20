c8a83efc823fa995cbb0282df8e860d44ca0c0d8
Lukmil <lmilasevicius@gmail.com>
2019-10-20 20:59:11 +0300

    [REFACT] grammar

--- app/utils/transactions-amount-counter.js
+++ app/utils/transactions-amount-counter.js
@@ -5,7 +5,7 @@ function sinceStartOfWeek(endDate, transactions) {
   if (Number.isNaN(Date.parse(endDate))) throw new Error('end date is not valid');
   if (!transactions || !transactions.length) return 0;
 
-  const startOfWeekDay = moment(endDate).startOf('isoWeek'); // monday is a first day of week
+  const startOfWeekDay = moment(endDate).startOf('isoWeek'); // monday is the first day of week
 
   return transactions
     .filter(({ date }) => moment(date).isBetween(startOfWeekDay, endDate, null, '[]')) // monday 00:00:00.000 is in week range
